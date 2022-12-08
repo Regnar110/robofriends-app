f60a8c700a671cc86844536e140850139f8a643e
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-08 22:50:51 +0100

    Added fetch method to gather users object from API

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './app.scss';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox.js';
-import {robots} from './components/robots';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    } 
+
+    componentDidMount() { // metoda cyklu życia komponentu react - działa po zamontowaniu się komponentu.
+        fetch('https://jsonplaceholder.cypress.io/users')
+        .then(response => response.json())  // metoda .json() jest metodą obiektu response. Robi to samo co JSON.parse() jednakże nie jest jak parse()  metodą statyczną obiektu JSON a metodą obiektu RESPONSE i działa tylko z fetch())
+        .then(users =>this.setState({robots:users}))
     }
 
     onSearchChange = (event) => {
@@ -20,13 +25,14 @@ class App extends Component {
     render() {
         const {robots} = this.state
         const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        return robots.length === 0 ? 
+        <h1 className='tc f1'>Loading</h1>
+        :
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+        </div>
     }
 }
 

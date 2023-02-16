86a7ff40f7e71a7011dc75262f02dcda6c1cd5a3
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-02-17 00:38:20 +0100

    implementet functional components - hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.scss';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    } 
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState("")
+    const [count, setCount] = useState(0)
 
-    componentDidMount() { // metoda cyklu życia komponentu react - działa po zamontowaniu się komponentu.
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.cypress.io/users')
         .then(response => response.json())  // metoda .json() jest metodą obiektu response. Robi to samo co JSON.parse() jednakże nie jest jak parse()  metodą statyczną obiektu JSON a metodą obiektu RESPONSE i działa tylko z fetch())
-        .then(users =>this.setState({robots:users}))
+        .then(users => setRobots(users))
+        console.log(count)
+    },[count]) // w HOOKU useEffect drugi parametr to tablica, która przyjmuje wartości
+    // które wskazują hookowi efektu kiedy ma się on uruchoimić. Na przykład
+    // jeżeli wpiszemyw niego searchfield to hoook ten uruchamiać się będzie tylko
+    //wtedy kiedy wartość searchfield będzie zmieniana np przy użyciu funkcji zmiany stanu
+    // Pozostawienie pustej [] da znak hookowi że ma się uruchomić tylko raz
+    // gdy komponent zostanie pierwszy raz zamontowany. Inaczej ComponentDidMount
+    // bez tego hook uruchamiał by się w nieskończonośc bo w jego wnętrzu zmieniamy
+    // stan robots.
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-
-    render() {
-        const {robots, searchField} = this.state
-        const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
-        return !robots.length ? 
+    return !robots.length ? 
         <h1 className='tc f1'>Loading</h1>
         :
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
+            <button onClick={()=> setCount(count+1)}>Click Me!</button>
+            <SearchBox searchChange={onSearchChange}/>
             <Scroll>
               <CardList robots={filteredRobots}/>
             </Scroll>
         </div>
-    }
 }
-
 export default App;
